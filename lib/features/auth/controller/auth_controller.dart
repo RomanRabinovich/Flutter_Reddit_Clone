285658133f8e1ceb5fb0c7f96e98c370b52393d8
roman.k <roman.k@3sda-evayadatasystems.com>
2022-10-23 15:47:22 +0300

    Loader added

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_4/core/utils.dart';
 import 'package:flutter_application_4/features/auth/repository/auth_repository.dart';
@@ -14,16 +13,6 @@ final authControllerProvider = StateNotifierProvider<AuthController, bool>(
   ),
 );
 
-final authStateChangeProvider = StreamProvider((ref) {
-  final authController = ref.watch(authControllerProvider.notifier);
-  return authController.authStateChange;
-});
-
-final getUserDataProvider = StreamProvider.family((ref, String uid) {
-  final authController = ref.watch(authControllerProvider.notifier);
-  return authController.getUserData(uid);
-});
-
 class AuthController extends StateNotifier<bool> {
   final AuthRepository _authRepository;
   final Ref _ref;
@@ -32,20 +21,13 @@ class AuthController extends StateNotifier<bool> {
         _ref = ref,
         super(false); // loading
 
-  Stream<User?> get authStateChange => _authRepository.authStateChange;
-
   void signInWithGoogle(BuildContext context) async {
     state = true;
     final user = await _authRepository.signInWithGoogle();
     state = false;
     user.fold(
-      (l) => showSnackBar(context, l.message),
-      (userModel) =>
-          _ref.read(userProvider.notifier).update((state) => userModel),
-    );
-  }
-
-  Stream<UserModel> getUserData(String uid) {
-    return _authRepository.getUserData(uid);
+        (l) => showSnackBar(context, l.message),
+        (userModel) =>
+            _ref.read(userProvider.notifier).update((state) => userModel));
   }
 }
