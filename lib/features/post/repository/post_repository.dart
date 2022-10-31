3d294dff7514e6efae517b8da649c765aaadbeb7
roman.k <roman.k@3sda-evayadatasystems.com>
2022-10-31 15:06:44 +0200

    Add post function was created

---
+++ lib/features/post/repository/post_repository.dart
@@ -0,0 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_application_4/core/constants/firebase_constants.dart';
+import 'package:flutter_application_4/core/failure.dart';
+import 'package:flutter_application_4/core/providers/firebase_providers.dart';
+import 'package:flutter_application_4/core/type_defs.dart';
+import 'package:flutter_application_4/models/post_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
+
+final postRepositoryProvider = Provider((ref) {
+  return PostRepository(
+    firestore: ref.watch(firestoreProvider),
+  );
+});
+
+class PostRepository {
+  final FirebaseFirestore _firestore;
+  PostRepository({required FirebaseFirestore firestore})
+      : _firestore = firestore;
+
+  CollectionReference get _posts =>
+      _firestore.collection(FirebaseConstants.postsCollection);
+
+  FutureVoid addPost(Post post) async {
+    try {
+      return right(_posts.doc(post.id).set(post.toMap()));
+    } on FirebaseException catch (e) {
+      throw e.message!;
+    } catch (e) {
+      return left(Failure(e.toString()));
+    }
+  }
+}
